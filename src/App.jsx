cd6624198645ea3c5749b15538806a28a36a8e03
harshkumarjangir <harshkumarjangir2002@gmail.com>
2025-12-09 10:55:56 +0530

    uncommented navbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Contact from "./pages/Contact"
 const App = () => {
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
