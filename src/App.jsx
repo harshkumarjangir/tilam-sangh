a6b41c133e3af6a679d3e58713350dd6772a21ad
Gaurav22-github <rj14gaurav@gmail.com>
2025-12-10 14:49:23 +0530

    added infrastructure page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
 import Tenders from "./pages/Tenders"
+import Infrastructure from "./pages/Infrastructure"
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/tenders" element={<Tenders/>}/>
+        <Route path="/infrastructure" element={<Infrastructure/>}/>
       </Routes>
       <Footer />
 
